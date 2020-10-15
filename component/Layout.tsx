2b999b1037e9a4974d562590dfd90a45205d4791
Okubo <aspastrana990@gmail.com>
2020-10-15 13:14:15 -0300

    Head y titulo

--- component/Layout.tsx
+++ component/Layout.tsx
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Pagina Personal</title>
+      </Head>
       <Navbar />
       <main className="container">{children}</main>
       <Footer />
