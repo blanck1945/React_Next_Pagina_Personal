96202413f45108a40b9a4116739cacf4b0794c58
Okubo <aspastrana990@gmail.com>
2020-11-02 02:19:19 -0300

    Arreglos y suma de proyectos

--- Api/Routes.ts
+++ Api/Routes.ts
@@ -1,5 +1,5 @@
 const prefix =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3500/api/"
     : "https://sq-heroku-react.herokuapp.com/api/";
 
